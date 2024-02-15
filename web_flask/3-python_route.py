cf0a32502a67ddd9f5d56ce7ddaf4bc296552285
Wonders Victor <wondersprince1@gmail.com>
2024-02-15 21:47:54 +0100

    Task 4

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -17,8 +17,16 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    '''display “C ”, followed by the value of the text variable'''
     text = text.replace('_', ' ')
     return f'C {text}'
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    '''display “Python ”, followed by the value of the text variable'''
+    text = text.replace('_', ' ')
+    return f'Python {text}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -17,8 +17,21 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    '''display “C ”, followed by the value of the text variable'''
     text = text.replace('_', ' ')
     return f'C {text}'
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    '''display “Python ”, followed by the value of the text variable'''
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    '''display “n is a number” only if n is an integer'''
+    return f'{n} is a number'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
