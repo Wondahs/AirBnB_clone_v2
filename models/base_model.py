d669dae64458a3525cbe738baea0c51d52380bcd
Wonders Victor <wondersprince1@gmail.com>
2024-02-17 07:16:58 +0100

    Fixed errors

--- models/base_model.py
+++ models/base_model.py
@@ -20,29 +20,31 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            
         else:
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs['updated_at'],
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-            else:  
+                        '%Y-%m-%dT%H:%M:%S.%f')
+            else:
                 self.updated_at = datetime.utcnow()
-            
+
             if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                        '%Y-%m-%dT%H:%M:%S.%f')
             else:
                 self.created_at = datetime.utcnow()
             if kwargs.get('__class__', None):
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
-            if self.__dict__.get('_sa_instance_state', None):
-                del self.__dict__['_sa_instance_state']
+            self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        if dictionary.get('_sa_instance_state', None):
+            del dictionary['_sa_instance_state']
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, dictionary)
 
     def save(self):
         from models import storage
@@ -54,14 +56,16 @@ class BaseModel:
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
-        self.__dict__.pop('_sa_instance_state', None)
+        #self.__dict__.pop('_sa_instance_state', None)
         dictionary.update(self.__dict__)
+        if dictionary.get('_sa_instance_state', None):
+            del dictionary['_sa_instance_state']
         dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+            (str(type(self)).split('.')[-1]).split('\'')[0]})
+        del dictionary['created_at'] #= self.created_at.isoformat()
+        del dictionary['updated_at'] #= self.updated_at.isoformat()
         return dictionary
-    
+
     def delete(self):
         from models import storage
         """deletes the current instance from the storage (models.storage)
