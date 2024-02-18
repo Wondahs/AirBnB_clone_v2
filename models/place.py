ce88fe2f2aadfe598581e4e1d207ac815e95a76c
Wonders Victor <wondersprince1@gmail.com>
2024-02-18 05:37:57 +0100

    more fixes

--- models/place.py
+++ models/place.py
@@ -37,23 +37,38 @@ class Place(BaseModel, Base):
     fkey -- Foriegn key    
     """
 
-    if getenv("HBNB_TYPE_STORAGE", None) != "db"
-    __tablename__ = "places"
-    city_id = Column(String(60),ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60),ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float) 
-    longitude = Column(Float)
-    reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship("Amenity", secondary=association_table,
-                             viewonly=False)
-    amenity_ids = []
+    if getenv("HBNB_TYPE_STORAGE", None) == "db":
+        __tablename__ = "places"
+        city_id = Column(String(60),ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60),ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0)
+        number_bathrooms = Column(Integer, default=0)
+        max_guest = Column(Integer, default=0)
+        price_by_night = Column(Integer, default=0)
+        latitude = Column(Float) 
+        longitude = Column(Float)
+        reviews = relationship("Review", backref="place", cascade="delete")
+        amenities = relationship("Amenity", secondary=association_table,
+                                viewonly=False)
+        amenity_ids = []
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
 
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
     def __init__(self, *args, **kwargs):
         """initializes city"""
         super().__init__(*args, **kwargs)
